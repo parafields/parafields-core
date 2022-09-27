3ac8b44f22b1b681d6428d7e0b5c6110f3fb7eb3
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2022-09-27 10:12:56 +0200

    Write Catch2 test

--- test/fieldtest.cc
+++ test/fieldtest.cc
@@ -1,7 +1,57 @@
 #include <catch2/catch.hpp>
-#include <mpi.h>
 #include <parafields/randomfield.hh>
+
+#include <limits>
 
 #include "traits.hh"
 
-TEST_CASE("Default 2D field generation", "[seq]") {}
+TEMPLATE_TEST_CASE("Default 1D field generation", "[seq]", float, double)
+{
+  // Define the configuration
+  Dune::ParameterTree config;
+  config["grid.cells"] = "32";
+  config["grid.extensions"] = "1";
+  config["stochastic.variance"] = "1";
+  config["stochastic.corrLength"] = "0.05";
+  config["stochastic.covariance"] =
+    GENERATE("exponential", "gaussian", "spherical");
+
+  // Instantiate the field
+  using Field = parafields::RandomField<GridTraits<TestType, TestType, 1>>;
+  Field field(config);
+  field.generate();
+}
+
+TEMPLATE_TEST_CASE("Default 2D field generation", "[seq]", float, double)
+{
+  // Define the configuration
+  Dune::ParameterTree config;
+  config["grid.cells"] = "16 16";
+  config["grid.extensions"] = "1 1";
+  config["stochastic.variance"] = "1";
+  config["stochastic.corrLength"] = "0.05";
+  config["stochastic.covariance"] =
+    GENERATE("exponential", "gaussian", "spherical");
+
+  // Instantiate the field
+  using Field = parafields::RandomField<GridTraits<TestType, TestType, 2>>;
+  Field field(config);
+  field.generate();
+}
+
+TEMPLATE_TEST_CASE("Default 3D field generation", "[seq]", float, double)
+{
+  // Define the configuration
+  Dune::ParameterTree config;
+  config["grid.cells"] = "8 8 8";
+  config["grid.extensions"] = "1 1 1";
+  config["stochastic.variance"] = "1";
+  config["stochastic.corrLength"] = "0.05";
+  config["stochastic.covariance"] =
+    GENERATE("exponential", "gaussian", "spherical");
+
+  // Instantiate the field
+  using Field = parafields::RandomField<GridTraits<TestType, TestType, 3>>;
+  Field field(config);
+  field.generate();
+}
